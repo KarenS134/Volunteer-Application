b0540b008925eeb027dca7c7c816d59159c5cfea
Peanut <ks1212735@swccd.edu>
2025-12-07 09:36:41 -0800

    Updated

--- src/main/java/com/application/volunteerapplication/HelloController.java
+++ src/main/java/com/application/volunteerapplication/HelloController.java
@@ -24,6 +24,23 @@ import java.net.URL;
 import java.sql.SQLOutput;
 import java.util.ResourceBundle;
 
+/**
+ * UML DIAGRAM:
+ * -----------------------------
+ * HelloController
+ * -----------------------------
+ * - myTextField : TextField
+ * - messageLabel : Label
+ * - messageSubmit : Label
+ * - positions : ComboBox<String>
+ * -----------------------------
+ * # onHelloButtonClick(Event event) :void
+ * + initialize(URL location, ResourceBundle resources) : void, throws IOException
+ * # onMapButtonClick(Event event) : void, throws IOException
+ * # onEventButtonClick(Event event) : void, throws IOException
+ * # onSubmitButtonClick(Event event) : void, throws IOException
+ * + validateInputOnEnter(ActionEvent event) : void,
+ */
 
 public class HelloController implements Initializable{
 
@@ -33,6 +50,7 @@ public class HelloController implements Initializable{
     @FXML
     private Label welcomeText;
 
+    /*When clicking the Application button it will open up a new window for the user to start a new application*/
     @FXML
     protected void onHelloButtonClick(Event event) throws IOException {
         Node source = (Node) event.getSource();
@@ -47,15 +65,14 @@ public class HelloController implements Initializable{
 
     }
 
-    @FXML
-    private ComboBox<String> positions;
-
+    /*This is where there is an Array where the user will be able to pick from their options of positions that are possible*/
     @Override
     public void initialize(URL location, ResourceBundle resources){
         String [] items = {"Organizer", "Chaperone","Animal Caretaker","Cleaner"};
         positions.getItems().addAll(items);
     }
 
+    /*When in the Home scene the map buttons will open up a new window for the user to see the map of this National Park (Note: Note not a real national park)*/
     @FXML
     protected void onMapButtonClick(Event event) throws IOException {
         Node source = (Node) event.getSource();
@@ -70,6 +87,7 @@ public class HelloController implements Initializable{
 
     }
 
+    /*In the Home scene there is also a button to see the local events you can do in the national park so this here will open up the scene in a new window where users are able to see the many events hosted*/
     @FXML
     protected void onEventButtonClick(Event event) throws IOException {
         Node source = (Node) event.getSource();
@@ -82,6 +100,7 @@ public class HelloController implements Initializable{
         stage.show();
     }
 
+    /*In the Volunteer Application scene when the user is ready to hit submit they will be shown a congratulation screen in where they have successfully submitted their form*/
     @FXML
     protected void onSubmitButtonClick(Event event) throws IOException {
         Node source = (Node) event.getSource();
@@ -94,6 +113,8 @@ public class HelloController implements Initializable{
         stage.show();
     }
 
+    /**Instance Variables**/
+
     @FXML
     private TextField myTextField;
 
@@ -103,6 +124,11 @@ public class HelloController implements Initializable{
     @FXML
     private Label messageSubmit;
 
+    /*In the Volunteer Application scene there is a combobox where the user is able to select their prefered postion in which they want to apply to*/
+    @FXML
+    private ComboBox<String> positions;
+
+    /**Implementing Email.java class into the textfield so that when users input their email wrong it will give an invalid error and if it is valid it will give a success message**/
     @FXML
     public void validateInputOnEnter(ActionEvent event) {
         String userInput = myTextField.getText();
